9d18a64a4a195edb6c37b574c0ddaeacbdfeec69
Nehemiah <nehemiah@hometalk.com>
2018-10-21 12:15:12 +0300

    Updates

---
+++ precache-manifest.4f525e9b4d30adf6da253deec5831711.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactmemory/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "723ddd8e19e8447d4272",
+    "url": "/reactmemory/static/js/runtime~main.723ddd8e.js"
+  },
+  {
+    "revision": "5ced87690c65bb1925bd",
+    "url": "/reactmemory/static/js/main.5ced8769.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/reactmemory/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "5ced87690c65bb1925bd",
+    "url": "/reactmemory/static/css/main.09f1cfb3.chunk.css"
+  },
+  {
+    "revision": "ba209a785c99b4f3ae5a99fb7ad4908e",
+    "url": "/reactmemory/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.9c05bf944907f95165e8b3ad6f1b2ef1.js"
+  "/reactmemory/precache-manifest.4f525e9b4d30adf6da253deec5831711.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-gh-pages/index.html", {
+workbox.routing.registerNavigationRoute("/reactmemory/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
